2c9e730e12eb20cc07a2b9fdcb8a0aeb233bce45
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-19 12:21:38 +0200

    custom css for navlinks if on mobile

--- src/app/components/navbar/navitems.tsx
+++ src/app/components/navbar/navitems.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import tw from "twin.macro";
 import { slide as Menu } from "react-burger-menu";
 import { useMediaQuery } from "react-responsive";
@@ -13,19 +13,29 @@ const ListContainer = styled.ul`
     `};
 `;
 
-const NavItem = styled.li`
+const NavItem = styled.li<{ menu?: any }>`
   ${tw`
-        text-sm
-        md:text-base
-        text-black
-        font-medium
-        mr-1
-        md:mr-5 
-        cursor-pointer
-        transition
-        duration-300 
-        ease-in-out
-        hover:text-gray-700
+    text-sm
+    md:text-base
+    text-black
+    font-medium
+    mr-1
+    md:mr-5
+    cursor-pointer
+    transition
+    duration-300
+    ease-in-out
+    hover:text-gray-700
+  `};
+  ${({ menu }) =>
+    menu &&
+    css`
+      ${tw`
+      text-white
+      text-xl
+      mb-3
+      focus:text-white
+    `};
     `};
 `;
 
@@ -36,16 +46,16 @@ export function NavItems() {
     return (
       <Menu right styles={menuStyles}>
         <ListContainer>
-          <NavItem>
+          <NavItem menu>
             <a href="#">Home</a>
           </NavItem>
-          <NavItem>
+          <NavItem menu>
             <a href="#">Cars</a>
           </NavItem>
-          <NavItem>
+          <NavItem menu>
             <a href="#">Services</a>
           </NavItem>
-          <NavItem>
+          <NavItem menu>
             <a href="#">Contact Us</a>
           </NavItem>
         </ListContainer>
