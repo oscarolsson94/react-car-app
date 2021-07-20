cfa56ff8ff2af50436f4c240dfea9f5318908ca2
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-20 10:57:14 +0200

    basic button styling

--- src/app/components/button/index.tsx
+++ src/app/components/button/index.tsx
@@ -1,8 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
 
 interface IButtonProps {
   theme: "filled" | "outlined";
   text: string;
 }
+
+const BaseButton = styled.button`
+  ${tw`
+        px-5 
+        py-3 
+        outline-none
+        rounded-md
+        text-white
+        text-xs
+        font-semibold
+        border-transparent
+        border-2 
+        border-solid
+        focus:outline-none
+        transition-all 
+        duration-200 
+        ease-in-out
+        m-1
+    `}
+`;
 
 export function Button(props: IButtonProps) {}
